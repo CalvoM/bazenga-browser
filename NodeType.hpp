5ca826c461bff0e0ced03c0f1a7cab3efb3aae97
CalvoM <mwadimemakokha@ieee.org>
2020-10-21 18:43:30 +0300

    Implement node types

---
+++ NodeType.hpp
@@ -0,0 +1,12 @@
+#ifndef NODE_TYPE_HPP_
+#define NODE_TYPE_HPP_
+/**
+ * @brief NodeType Base class returns the type of Node
+ * (https://dom.spec.whatwg.org/#dom-node-nodetype)
+ *
+ */
+class NodeType {
+public:
+    virtual ~NodeType();
+};
+#endif;
